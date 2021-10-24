1d00f1f63a428cafbb2005c2485e7d9f3a658290
Sharon Cherop <sh.cherop@gmail.com>
2021-10-24 23:03:47 +0300

    migrated the first time

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class Project(models.Model):
+    title=models.CharField(max_length=30)
+    image=models.ImageField()
+    description=models.TextField()
+    link=models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Profile(models.Model):
+    picture=models.ImageField()
+    bio=models.TextField(max_length=100)
+    projects=models.ForeignKey('Project',related_name='links',on_delete=models.CASCADE)
+    contact=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.picture
