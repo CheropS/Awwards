51f643ed7c0fb80d4e5d0439952e8c0b3566920c
Sharon Cherop <sh.cherop@gmail.com>
2021-11-05 10:21:33 +0300

    first test case passed

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Profile, Project
 
 # Create your tests here.
+class ProjectTestClass(TestCase):
+    #set up method
+    def setUp(self):
+        self.sharry=Project(author='sharry', title='Instaclone', description='app looks like instagram')
